1aa6919252a923115b65f572503b76d6fd766b8b
Davi Wohlcke Thiengo <109366313+dwthiengo@users.noreply.github.com>
2022-11-30 10:06:18 -0300

    Add files via upload

---
+++ src/velhajogo/VelhaJogo.java
@@ -0,0 +1,18 @@
+package velhajogo;
+
+/**
+ *
+ * @author dwthiengo
+ */
+public class VelhaJogo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Jogo FrameJogo = new Jogo();
+        FrameJogo.setVisible(true);
+        
+    }
+    
+}
